b69df550a18cc6d5767558d9ae53c5c94c81df13
Eber27 <eberfreire@gmail.com>
2021-03-30 18:02:57 -0500

    Static_Exerc

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Celsius = TempConverter.FahrenheitToCelsius(25);
+            Console.WriteLine($"This is {Celsius} degrees celsius.");
+
+            var Fahrenheit = TempConverter.CelsiusToFahrenheit(40);
+            Console.WriteLine($"This is {Fahrenheit} degrees fahrenheit");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double Fahrenheit)
+        {
+            var degreesCelsius = (Fahrenheit - 32) / 1.8;
+            return degreesCelsius;
+        }
+
+        public static double CelsiusToFahrenheit(double Celsius)
+        {
+            var degreesFahrenheit = (Celsius * 9) / 5 + 32;
+            return degreesFahrenheit;
+        }
+    }
+}
